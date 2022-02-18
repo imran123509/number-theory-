ae7bafc14d079ed05467a04c418297f196b907ad
Imran hassan <72485040+imran123509@users.noreply.github.com>
2022-02-18 23:13:52 +0530

    Add files via upload

---
+++ printing binary numbers.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+string to_binary(int x) {
+	string s;
+	while(x > 0) {
+		s += (x % 2 ? '1' : '0');
+		x /= 2;
+	}
+	reverse(s.begin(), s.end());
+	return s;
+}
+
+int main() {
+	cout << "13 = " << to_binary(13) << endl; // 1101
+	
+	int x = 53;
+	int y = 28;
+	cout << "x = " << to_binary(x) << ", y = " << to_binary(y) << endl;
+	}
